570c63047506d79605f4a55b35519c7fbbeaf2a7
vages <eirikvageskar@dhcp-065097.wlan.ntnu.no>
2015-02-09 14:03:46 +0100

    Add simplify functions to all special functions

--- ps3/src/simplifynodes.c
+++ ps3/src/simplifynodes.c
@@ -17,6 +17,7 @@ Node_t* simplify_types ( Node_t *root, int depth )
 	if(outputStage == 4)
 		printf( "%*cSimplify %s \n", depth, ' ', root->nodetype.text );
 
+	root = simplify_default(root, depth);
 }
 
 
@@ -25,7 +26,7 @@ Node_t* simplify_function ( Node_t *root, int depth )
 	if(outputStage == 4)
 		printf( "%*cSimplify %s \n", depth, ' ', root->nodetype.text );
 
-
+	root = simplify_default(root, depth);
 }
 
 
@@ -36,7 +37,7 @@ Node_t* simplify_declaration_statement ( Node_t *root, int depth )
 	if(outputStage == 4)
 		printf( "%*cSimplify %s \n", depth, ' ', root->nodetype.text );
 
-
+	root = simplify_default(root, depth);
 }
 
 
@@ -45,7 +46,7 @@ Node_t* simplify_single_child ( Node_t *root, int depth )
 	if(outputStage == 4)
 		printf( "%*cSimplify %s \n", depth, ' ', root->nodetype.text );
 
-
+	root = simplify_default(root, depth);
 }
 
 Node_t* simplify_list_with_null ( Node_t *root, int depth )
@@ -53,7 +54,7 @@ Node_t* simplify_list_with_null ( Node_t *root, int depth )
 	if(outputStage == 4)
 		printf( "%*cSimplify %s \n", depth, ' ', root->nodetype.text );
 
-
+	root = simplify_default(root, depth);
 }
 
 
@@ -62,7 +63,7 @@ Node_t* simplify_list ( Node_t *root, int depth )
 	if(outputStage == 4)
 		printf( "%*cSimplify %s \n", depth, ' ', root->nodetype.text );
 	
-
+	root = simplify_default(root, depth);
 }
 
 
@@ -71,6 +72,6 @@ Node_t* simplify_expression ( Node_t *root, int depth )
 	if(outputStage == 4)
 		printf( "%*cSimplify %s (%s) \n", depth, ' ', root->nodetype.text, root->expression_type.text );
 
-
+	root = simplify_default(root, depth);
 }
 
