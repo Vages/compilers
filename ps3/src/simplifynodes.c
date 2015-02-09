ec78654d54a941eba81e62abafac532ed3f19a8c
vages <eirikvageskar@dhcp-065097.wlan.ntnu.no>
2015-02-09 13:56:23 +0100

    Make function definitions consequent

--- ps3/src/simplifynodes.c
+++ ps3/src/simplifynodes.c
@@ -12,7 +12,7 @@ Node_t* simplify_default ( Node_t *root, int depth )
 }
 
 
-Node_t *simplify_types ( Node_t *root, int depth )
+Node_t* simplify_types ( Node_t *root, int depth )
 {
 	if(outputStage == 4)
 		printf( "%*cSimplify %s \n", depth, ' ', root->nodetype.text );
@@ -20,18 +20,7 @@ Node_t *simplify_types ( Node_t *root, int depth )
 }
 
 
-Node_t *simplify_function ( Node_t *root, int depth )
-{
-	if(outputStage == 4)
-		printf( "%*cSimplify %s \n", depth, ' ', root->nodetype.text );
-
-
-}
-
-
-
-
-Node_t *simplify_declaration_statement ( Node_t *root, int depth )
+Node_t* simplify_function ( Node_t *root, int depth )
 {
 	if(outputStage == 4)
 		printf( "%*cSimplify %s \n", depth, ' ', root->nodetype.text );
@@ -40,7 +29,9 @@ Node_t *simplify_declaration_statement ( Node_t *root, int depth )
 }
 
 
-Node_t *simplify_single_child ( Node_t *root, int depth )
+
+
+Node_t* simplify_declaration_statement ( Node_t *root, int depth )
 {
 	if(outputStage == 4)
 		printf( "%*cSimplify %s \n", depth, ' ', root->nodetype.text );
@@ -48,7 +39,16 @@ Node_t *simplify_single_child ( Node_t *root, int depth )
 
 }
 
-Node_t *simplify_list_with_null ( Node_t *root, int depth )
+
+Node_t* simplify_single_child ( Node_t *root, int depth )
+{
+	if(outputStage == 4)
+		printf( "%*cSimplify %s \n", depth, ' ', root->nodetype.text );
+
+
+}
+
+Node_t* simplify_list_with_null ( Node_t *root, int depth )
 {
 	if(outputStage == 4)
 		printf( "%*cSimplify %s \n", depth, ' ', root->nodetype.text );
@@ -57,7 +57,7 @@ Node_t *simplify_list_with_null ( Node_t *root, int depth )
 }
 
 
-Node_t *simplify_list ( Node_t *root, int depth )
+Node_t* simplify_list ( Node_t *root, int depth )
 {
 	if(outputStage == 4)
 		printf( "%*cSimplify %s \n", depth, ' ', root->nodetype.text );
@@ -66,7 +66,7 @@ Node_t *simplify_list ( Node_t *root, int depth )
 }
 
 
-Node_t *simplify_expression ( Node_t *root, int depth )
+Node_t* simplify_expression ( Node_t *root, int depth )
 {
 	if(outputStage == 4)
 		printf( "%*cSimplify %s (%s) \n", depth, ' ', root->nodetype.text, root->expression_type.text );
