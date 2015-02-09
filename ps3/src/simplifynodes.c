f5b7b01dfb874304e735ad83e88594daac8bfe7f
vages <eirikvageskar@dhcp-065097.wlan.ntnu.no>
2015-02-09 18:04:17 +0100

    Add missing declaration

--- ps3/src/simplifynodes.c
+++ ps3/src/simplifynodes.c
@@ -126,7 +126,7 @@ Node_t* simplify_list ( Node_t *root, int depth )
 	
 	if (root->n_children > 1){
 		Node_t* left_child = root->children[0];
-		no_of_left_children = left_child->n_children;
+		int no_of_left_children = left_child->n_children;
 		Node_t** new_children = malloc(sizeof(Node_t*)*(no_of_left_children+1));
 		for(int i = 0; i<no_of_left_children; i++){
 			new_children[i] = left_child->children[i];
