41491db93a2f1297428c36ba6a4fdc1ea4f2cce4
vages <eirikvageskar@dhcp-065191.wlan.ntnu.no>
2015-02-11 13:29:28 +0100

    Comment out simplify list

--- ps3/src/simplifynodes.c
+++ ps3/src/simplifynodes.c
@@ -119,9 +119,12 @@ Node_t* simplify_list_with_null ( Node_t *root, int depth )
 
 Node_t* simplify_list ( Node_t *root, int depth )
 {
+	
 	if(outputStage == 4)
 		printf( "%*cSimplify %s \n", depth, ' ', root->nodetype.text );
 	
+
+	/*
 	root = simplify_default(root, depth);
 	
 	if (root->n_children > 1){
@@ -138,6 +141,8 @@ Node_t* simplify_list ( Node_t *root, int depth )
 		root->children = new_children;
 		root->n_children = no_of_left_children+1;
 	}
+	*/
+	root = simplify_default(root, depth+1);
 
 	return root;
 }
