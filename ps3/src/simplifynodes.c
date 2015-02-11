cf5dde2503c51473f7e909c3593ff81a6c094db3
vages <eirikvageskar@dhcp-065191.wlan.ntnu.no>
2015-02-11 14:38:51 +0100

    Remove draft for simplify list

--- ps3/src/simplifynodes.c
+++ ps3/src/simplifynodes.c
@@ -167,24 +167,6 @@ Node_t* simplify_list ( Node_t *root, int depth )
 		root->children = new_children;
 		root->n_children = no_of_children_in_left+1;
 	}
-	/*
-	root = simplify_default(root, depth);
-	
-	if (root->n_children > 1){
-		Node_t* left_child = root->children[0];
-		int no_of_left_children = left_child->n_children;
-		fprintf(stderr, "%d\n", no_of_left_children);
-		Node_t** new_children = malloc(sizeof(Node_t*)*(no_of_left_children+1));
-		for(int i = 0; i<no_of_left_children; i++){
-			new_children[i] = left_child->children[i];
-		}
-		new_children[no_of_left_children] = root->children[0];
-
-		free(root->children);
-		root->children = new_children;
-		root->n_children = no_of_left_children+1;
-	}
-	*/
 
 	return root;
 }
