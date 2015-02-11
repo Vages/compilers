ec8225d460d0512cf62626339652a15180a4ef3e
vages <eirikvageskar@dhcp-065191.wlan.ntnu.no>
2015-02-11 13:35:57 +0100

    Add simplify single child function

--- ps3/src/simplifynodes.c
+++ ps3/src/simplifynodes.c
@@ -100,7 +100,11 @@ Node_t* simplify_single_child ( Node_t *root, int depth )
 	if(outputStage == 4)
 		printf( "%*cSimplify %s \n", depth, ' ', root->nodetype.text );
 
-	root = simplify_default(root, depth+1);
+	root->children[0] = root->children[0]->simplify(root->children[0], depth+1);
+
+	Node_t* child = root->children[0];
+	free (root);
+	root = child;
 
 	return root;
 
