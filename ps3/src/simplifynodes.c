623384410361c32ba09bf316329946ac1bf3a535
vages <eirikvageskar@dhcp-065191.wlan.ntnu.no>
2015-02-11 13:16:48 +0100

    Add debug code for simplify list

--- ps3/src/simplifynodes.c
+++ ps3/src/simplifynodes.c
@@ -129,6 +129,7 @@ Node_t* simplify_list ( Node_t *root, int depth )
 		int no_of_left_children = left_child->n_children;
 		Node_t** new_children = malloc(sizeof(Node_t*)*(no_of_left_children+1));
 		for(int i = 0; i<no_of_left_children; i++){
+			fprintf(stderr, i);
 			new_children[i] = left_child->children[i];
 		}
 		new_children[no_of_left_children] = root->children[0];
