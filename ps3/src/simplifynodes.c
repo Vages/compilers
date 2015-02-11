25c8d4a0b657e0d67b43705088964ee295aaeb89
vages <eirikvageskar@dhcp-065191.wlan.ntnu.no>
2015-02-11 14:35:15 +0100

    Add simplify default statement

--- ps3/src/simplifynodes.c
+++ ps3/src/simplifynodes.c
@@ -150,6 +150,7 @@ Node_t* simplify_list ( Node_t *root, int depth )
 	if (root->n_children == 1){
 		root = simplify_default(root, depth+1);
 	} else if (root->n_children>1){
+		root = simplify_default(root, depth+1);
 		Node_t* left_child = root->children[0];
 		int no_of_children_in_left = left_child->n_children;
 
