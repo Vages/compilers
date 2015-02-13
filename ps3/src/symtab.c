95b1a86cb886777ceb23afa6c20ad3722d1b1d28
vages <eirikvageskar@dhcp-067030.wlan.ntnu.no>
2015-02-13 16:51:03 +0100

    Comment out last commit to see if it caused segf

--- ps3/src/symtab.c
+++ ps3/src/symtab.c
@@ -36,7 +36,7 @@ strings_add ( char *str )
 		strings = (char**)realloc(strings, strings_size*sizeof(char*));
 	}
 	strings[strings_index] = str;
-	fprintf(stderr, "%s", str);
+	//fprintf(stderr, "%s", str);
 	return strings_index;
 }
 
