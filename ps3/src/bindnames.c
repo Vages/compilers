5844d67c35de6c2e22f3b6843aa49c906c913823
vages <eirikvageskar@dhcp-067030.wlan.ntnu.no>
2015-02-13 16:58:51 +0100

    Add type check to bind_constant

--- ps3/src/bindnames.c
+++ ps3/src/bindnames.c
@@ -20,8 +20,10 @@ int bind_constant ( node_t *root, int stackOffset)
 {
 	if(outputStage == 6)
 		printf( "CONSTANT\n");
-
-	root->string_index = strings_add(root->string_const);
+	
+	if (root->data_type.base_type = STRING_TYPE){ 
+		root->string_index = strings_add(root->string_const);
+	}
 
 	return 0;
 }
