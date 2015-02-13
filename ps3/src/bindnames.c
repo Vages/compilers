4648693dbbe5e445b2ed4f172e291469e4a878a8
vages <eirikvageskar@dhcp-067030.wlan.ntnu.no>
2015-02-13 17:02:41 +0100

    Change back to arrow

--- ps3/src/bindnames.c
+++ ps3/src/bindnames.c
@@ -21,8 +21,8 @@ int bind_constant ( node_t *root, int stackOffset)
 	if(outputStage == 6)
 		printf( "CONSTANT\n");
 	
-		root.string_index = strings_add(root->string_const);
 	if (root->data_type.base_type == STRING_TYPE){ 
+		root->string_index = strings_add(root->string_const);
 	}
 
 	return 0;
