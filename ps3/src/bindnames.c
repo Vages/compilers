0c701f8b7d041f14378fa0f26b559ff864c20dde
vages <eirikvageskar@dhcp-067030.wlan.ntnu.no>
2015-02-13 15:35:21 +0100

    Change type to lower case

--- ps3/src/bindnames.c
+++ ps3/src/bindnames.c
@@ -5,7 +5,7 @@ char* thisClass;
 int bind_default ( node_t *root, int stackOffset)
 {
 	for (int i = 0; i < root->n_children; i++){
-		Node_t* child = root->children[i];
+		node_t* child = root->children[i];
 		if (child != NULL) {
 			(child->bind_names)(child, stackOffset+1);
 		}
