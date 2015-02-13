e96f9cc36a5dace851e1b8d82cd23c231729ca83
vages <eirikvageskar@dhcp-067030.wlan.ntnu.no>
2015-02-13 15:32:21 +0100

    Change variable name

--- ps3/src/bindnames.c
+++ ps3/src/bindnames.c
@@ -7,7 +7,7 @@ int bind_default ( node_t *root, int stackOffset)
 	for (int i = 0; i < root->n_children; i++){
 		Node_t* child = root->children[i];
 		if (child != NULL) {
-			child->bind(child, depth+1);
+			child->bind(child, stackOffset+1);
 		}
 	}
 
