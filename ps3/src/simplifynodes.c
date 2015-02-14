32cda5860c38757aa82b882add11752c23b11e70
vages <eirikvageskar@gmail.com>
2015-02-14 11:48:48 +0100

    Add a free statement

--- ps3/src/simplifynodes.c
+++ ps3/src/simplifynodes.c
@@ -170,6 +170,7 @@ Node_t* simplify_list_with_null ( Node_t *root, int depth )
 		new_children[0] = root->children[1];
 		free(root->children);
 		root->children = new_children;
+		free(new_children);
 		root->n_children = 1;
 		return root;
 	}
