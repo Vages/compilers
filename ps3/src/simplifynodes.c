23fb45a9b4cf878109b3e05fcdacc6a727d43082
vages <eirikvageskar@gmail.com>
2015-02-14 11:54:03 +0100

    Free some more children

--- ps3/src/simplifynodes.c
+++ ps3/src/simplifynodes.c
@@ -184,6 +184,7 @@ Node_t* simplify_list_with_null ( Node_t *root, int depth )
 	}
 
 	new_children[no_of_children_in_left] = root->children[1];
+	
 	node_finalize(left_child);
 	free(root->children);
 	root->children = new_children;
@@ -213,6 +214,7 @@ Node_t* simplify_list ( Node_t *root, int depth )
 
 		new_children[no_of_children_in_left] = root->children[1];
 		node_finalize(left_child);
+		free(children);
 		root->children = new_children;
 		root->n_children = no_of_children_in_left+1;
 	}
