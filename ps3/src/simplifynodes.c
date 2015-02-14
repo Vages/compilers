2f1a3b596a578987cee2fb40fcbb0cdb0bfb15b1
Eirik Vågeskar <eirikvageskar@gmail.com>
2015-02-14 11:06:29 +0100

    Try a new simplify types

--- ps3/src/simplifynodes.c
+++ ps3/src/simplifynodes.c
@@ -22,6 +22,24 @@ Node_t* simplify_types ( Node_t *root, int depth )
 
 	root = simplify_default(root, depth);
 
+	root->data_type.array_type = root->children[0].data_type.base_type;
+	node_finalize(root->children[0]);
+	
+	int index_children = root->children[1]->n_children;
+	root->data_type.n_dimensions = index_children;
+	root->data_type.dimensions = (int)malloc(sizeof(int)*index_children);
+	for (int j = 0; j < index_children; j++){
+		root->data_type.dimensions[j] = root->children[1]->children[j]->int_const;
+		node_finalize(root->children[1]->children[j]);
+	}
+	node_finalize(root->children[1]);
+
+	free(root->children);
+	root->n_children = 0;
+
+	return root;
+	
+	/*
 	if (root->data_type.base_type == ARRAY_TYPE){
 		int no_of_children = root->n_children;
 		for (int i = 0; i<no_of_children; i++){
@@ -44,7 +62,7 @@ Node_t* simplify_types ( Node_t *root, int depth )
 
 	root->n_children = 0;
 
-	return root;
+	return root;*/
 }
 
 
