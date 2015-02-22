3878171d37dcedc92ad675bf5a09e260aca6eb14
vages <eirikvageskar@gmail.com>
2015-02-22 23:10:28 +0100

    Fix another typo in expression

--- ps4/src/typecheck.c
+++ ps4/src/typecheck.c
@@ -100,7 +100,7 @@ data_type_t typecheck_expression(node_t* root)
                     int iseq = equal_types(l_child_dt, root->children[1]->data_type);
                     if (!iseq){
                         type_error(root);
-                    } else if ((l_child_dt.base_type != FLOAT_TYPE) || (l_child_dt.base_type != INT_TYPE)){
+                    } else if ((l_child_dt.base_type != FLOAT_TYPE) && (l_child_dt.base_type != INT_TYPE)){
                         type_error(root);
                     }
                     return l_child_dt;
