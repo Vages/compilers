3822ae74b01169885e6bfb3be294063c9cf756a3
vages <eirikvageskar@gmail.com>
2015-02-22 15:11:04 +0100

    Add another line of cleanup

--- ps4/src/bindnames.c
+++ ps4/src/bindnames.c
@@ -156,7 +156,7 @@ int bind_expression( node_t* root, int stackOffset)
 
 	if (root->expression_type.index == FUNC_CALL_E){
 		root->function_entry = function_get(root->children[0]->label);
-
+		stackOffset = bd(root->children[1], stackOffset);
 	} else {
 		stackOffset = bd(root, stackOffset);
 	}
