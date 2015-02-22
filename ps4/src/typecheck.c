ebeff200c9527f5fb1365c79a7b1ecba675ce7a4
vages <eirikvageskar@gmail.com>
2015-02-22 22:28:01 +0100

    Add type check variable

--- ps4/src/typecheck.c
+++ ps4/src/typecheck.c
@@ -155,7 +155,11 @@ data_type_t typecheck_expression(node_t* root)
 }
 
 data_type_t typecheck_variable(node_t* root){
-    
+    typecheck_default(root);
+
+    if(root->entry != NULL){
+        return root->entry->type;
+    }
 }
 
 data_type_t typecheck_assignment(node_t* root)
