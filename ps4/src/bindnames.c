f01183cc60680318ffd4cf9303442168c8d568d4
vages <eirikvageskar@gmail.com>
2015-02-23 13:43:06 +0100

    Add missing letter

--- ps4/src/bindnames.c
+++ ps4/src/bindnames.c
@@ -148,7 +148,7 @@ int bind_expression( node_t* root, int stackOffset)
 
 	if (root->expression_type.index == FUNC_CALL_E){
 		root->function_entry = function_get(root->children[0]->label);
-		printf(stderr, "%d", n_children);
+		fprintf(stderr, "%d", n_children);
 		stackOffset = bd(root->children[1], stackOffset);
 	} else {
 		stackOffset = bd(root, stackOffset);
