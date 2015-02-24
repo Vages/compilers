75162501e53632cdb6ff3d5aa2bd6a3b8cebb5cb
vages <eirikvageskar@gmail.com>
2015-02-24 18:09:40 +0100

    Remove rt_bool variable

--- ps4/src/typecheck.c
+++ ps4/src/typecheck.c
@@ -93,7 +93,6 @@ data_type_t typecheck_expression(node_t* root)
         {
             data_type_t l_child_dt;
             data_type_t r_child_dt;
-            data_type_t rt_bool = {.base_type = BOOL_TYPE};
 
             case ADD_E: case SUB_E: case DIV_E: case MUL_E:
                 ;
