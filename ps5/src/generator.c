2576ba8ba4261ae9108c7ab8b3e356a5d6676856
vages <eirikvageskar@gmail.com>
2015-03-16 15:56:35 +0100

    Add comment to declaration_statement

--- ps5/src/generator.c
+++ ps5/src/generator.c
@@ -170,7 +170,7 @@ void gen_DECLARATION_STATEMENT (node_t *root, int scopedepth)
 	*/
 	tracePrint("Starting DECLARATION: adding space on stack\n");
 
-	instruction_add(PUSH, r6, NULL, 0, 0);
+	instruction_add(PUSH, r6, NULL, 0, 0); // Make room for the variable on stack. 
 
 	tracePrint("Ending DECLARATION\n");
 }
