608b1ce541021a11a6f10fc022fcfd3d10bef7f8
vages <eirikvageskar@gmail.com>
2015-03-16 15:56:20 +0100

    Add first draft of gen_function
    A bit puzzled about storing return value in r0. Also a bit uncertain
    about popping to pc in last line.

--- ps5/src/generator.c
+++ ps5/src/generator.c
@@ -142,7 +142,17 @@ void gen_FUNCTION ( node_t *root, int scopedepth )
     scopedepth++;
     tracePrint ( "Starting FUNCTION (%s) with depth %d\n", root->label, scopedepth);
     
-    
+    // Eirik: Start of my own stuff.
+    instruction_add(LABEL, STRDUP(root->label), NULL, 0, 0);  // Make label.
+    instruction_add(PUSH, lr, NULL, 0, 0);  // Callee saves link register on stack.
+    instruction_add(PUSH, fp, NULL, 0, 0);  // Callee saves old fp on stack.
+    instruction_add(MOV, fp, sp, 0, 0);  // Callee sets new fp to top of stack.
+    gen_default(root->children[1], scopedepth);  // Generate code for function body (statement list, second child).
+    instruction_add(MOV, sp, fp);  // Callee sets sp to its fp.
+    instruction_add(POP, fp, NULL, 0, 0);  // Callee restores old fp.
+    // TODO: Slides say that we have to store return value in r0 here, but this has perhaps been done for us already.
+    instruction_add(POP, pc, NULL, 0, 0);  // Callee jumps back to caller by popping the return address.
+    //Eirik: End of my own stuff.
     
     tracePrint ("Leaving FUNCTION (%s) with depth %d\n", root->label, scopedepth);
     scopedepth--;      
