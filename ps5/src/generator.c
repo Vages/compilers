40cfb3728a1d4bda8cf5422bebee8565cecf576a
vages <eirikvageskar@gmail.com>
2015-03-17 17:18:56 +0100

    Add underscore to call of first function
    Should fix at least one problem

--- ps5/src/generator.c
+++ ps5/src/generator.c
@@ -128,7 +128,9 @@ void gen_PROGRAM ( node_t *root, int scopedepth)
 	print_start();
 	
 	// Eirik: Start of own stuff
-	instruction_add(BL, STRDUP(root->children[0]->children[0]->label), NULL, 0, 0);  // Insert a branch to the label of the first function in the function list
+	char* first_func_label;
+	sprintf(first_func_label, "_%s", root->children[0]->children[0]->label)
+	instruction_add(BL, first_func_label), NULL, 0, 0);  // Insert a branch to the label of the first function in the function list
 	// Eirik: End of own stuff
 
 	tracePrint("End PROGRAM\n");
