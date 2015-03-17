611439f2e0a4f6edbfe474eabb0ab1e7bf704a80
vages <eirikvageskar@gmail.com>
2015-03-17 13:27:51 +0100

    Push and pop registers in func_call_e

--- ps5/src/generator.c
+++ ps5/src/generator.c
@@ -187,13 +187,17 @@ void gen_EXPRESSION ( node_t *root, int scopedepth )
 
 	// Eirik: Start of own stuff
 	if (root->expression_type.index == FUNC_CALL_E){
-		instruction_add(STRING, STRDUP("\tpush {r1-r11, lr}"), NULL, 0, 0 ); // Save registers r1 to r11 to stack (we do not care about r0, because it is used for results)
+		instruction_add(STRING, STRDUP("\tpush {r1-r6}"), NULL, 0, 0 ); // Save registers r1 to r6 to stack (we do not care about r0, because it is used for results)
 		for (int i; i<root->children[1]->n_children; i++){
 			gen_default(root->children[1]->children[i], scopedepth);  // Generate code for each argument to the function (expression nodes)
 			instruction_add(PUSH, r0, NULL, 0, 0);  // Results of the expressions are assumed to be in r0. Push them to the stack.
 		}
 		instruction_add(BL, STRDUP(root->children[0]->label), NULL, 0, 0);  // Caller saves return address in link register and branches to function
 		instruction_add (STRING, STRDUP("\tpop {r1-r11, lr}"), NULL, 0, 0 );
+		for (int i; i<root->children[1]->n_children; i++){
+			instruction_add(pop, r6, NULL, 0, 0);  // We need to remove parameters from stack. This is one way to do it; another would be to manipulate sp directly.
+		}
+		instruction_add (STRING, STRDUP("\tpop {r1-r6, lr}"), NULL, 0, 0 ); // Restore registers. Assume that parent nodes use result.
 	}
 	else if (root->expression_type.index == ARRAY_INDEX_E){
 
