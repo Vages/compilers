6d25258c15746e567b5328df32c6d9415cc7ca70
vages <eirikvageskar@gmail.com>
2015-03-18 13:40:22 +0100

    Add back in some stuff

--- ps5/src/generator.c
+++ ps5/src/generator.c
@@ -143,7 +143,6 @@ void gen_FUNCTION ( node_t *root, int scopedepth )
     instruction_add(PUSH, fp, NULL, 0, 0);  // Callee saves old fp on stack. 
     instruction_add(MOV, fp, sp, 0, 0);  // Callee sets new fp to top of stack.
     gen_default(root->children[1], scopedepth);  // Generate code for function body (statement list is the second child).
-    
     instruction_add(MOV, sp, fp, 0, 0);  // Callee sets stack pointer to its frame pointer (removing all local variables).
     instruction_add(POP, fp, NULL, 0, 0);  // Callee restores old fp.
     // TODO: Slides say that we have to store return value in r0 here, but this has perhaps been done for us already.
@@ -194,18 +193,18 @@ void gen_EXPRESSION ( node_t *root, int scopedepth )
 	// Eirik: Start of own stuff
 	if (root->expression_type.index == FUNC_CALL_E){
 		
-		//instruction_add(STRING, STRDUP("\tpush {r1-r6, lr}"), NULL, 0, 0 ); // Save registers r1 to r6 to stack (we do not care about r0, because it is used for results)
+		instruction_add(STRING, STRDUP("\tpush {r1-r6}"), NULL, 0, 0 ); // Save registers r1 to r6 to stack (we do not care about r0, because it is used for results)
 		gen_default(root->children[1], scopedepth);  // Push parameters on stack
 		instruction_add(MOV, lr, pc, 0, 0); // Save return address in link register
 		char func_label[80]; 
 		sprintf(func_label, "_%s", root->children[0]->label);
 		instruction_add(BL, STRDUP(func_label), NULL, 0, 0);  // Caller saves return address in link register and branches to function
-		/*if (root->children[1] != NULL){
+		if (root->children[1] != NULL){
 			for (int i = 0; i<root->children[1]->n_children; i++){
 				instruction_add(POP, r6, NULL, 0, 0);  // We need to remove parameters from stack. This is one way to do it; another would be to manipulate sp directly.
 			}
-		}*/
-		//instruction_add(STRING, STRDUP("\tpop {r1-r6, lr}"), NULL, 0, 0 ); // Restore registers.
+		}
+		instruction_add(STRING, STRDUP("\tpop {r1-r6}"), NULL, 0, 0 ); // Restore registers.
 		instruction_add(PUSH, r0, NULL, 0, 0); // Push returned value to top of stack.
 
 	}
