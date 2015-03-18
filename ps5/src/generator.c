66494902f4d960d53139110d5612c7ebc579b2e6
vages <eirikvageskar@gmail.com>
2015-03-18 20:33:44 +0100

    Remove an expression

--- ps5/src/generator.c
+++ ps5/src/generator.c
@@ -294,7 +294,7 @@ void gen_RETURN_STATEMENT ( node_t *root, int scopedepth )
 	tracePrint ( "Starting RETURN_STATEMENT\n");
 	
 	// Eirik: Start of own stuff
-	gen_default(root->children[0], scopedepth);  // Generate code for right hand side. An expression, so result is on top of stack.
+	gen_default(root, scopedepth);  // Generate code for right hand side. An expression, so result is on top of stack.
 	instruction_add(POP, r0, NULL, 0, 0);  // Place result in r0, as slides describe.
 	// Eirik: End of own stuff
 
