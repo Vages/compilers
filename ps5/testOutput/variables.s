10772753f8ce1c1f6d566145233d546ca5c6e6b6
Vages <eirikvageskar@gmail.com>
2015-03-19 15:57:02 +0100

    Results

--- ps5/testOutput/variables.s
+++ ps5/testOutput/variables.s
@@ -46,8 +46,8 @@ _main:
 #15 End ASSIGNMENT_STATEMENT
 #16 Starting ASSIGNMENT_STATEMENT
 #17 Starting CONSTANT
-	movw	r0, #:lower16:Ð•‚ÿ
-	movt	r0, #:upper16:Ð•‚ÿ
+	movw	r0, #:lower16:À§0`ÿ
+	movt	r0, #:upper16:À§0`ÿ
 	push	{r0}
 #18 End CONSTANT
 	pop	{r0}
